067ec2ca07216c6ebee0f80399fc382f91df62c2
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-25 23:29:48 -0400

    Using iterable methods

--- bin/reading_elements.dart
+++ bin/reading_elements.dart
@@ -1,6 +1,13 @@
 void main() {
   var iterable = ['Salad', 'Popcorn', 'Toast'];
+//  Using a for-in loop
   for (var element in iterable) {
     print(element);
   }
+//  Using first and last
+  print('The first element is ${iterable.first}');
+  print('The last element is ${iterable.last}');
+//  Using firstWhere()
+  var element = iterable.firstWhere((element) => element.length > 5);
+  print(element);
 }
