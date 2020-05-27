c6160ae130518bcae2083f9f7a936dcf729f1ee9
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-26 23:50:26 -0400

    Filtering

---
+++ bin/fitering.dart
@@ -0,0 +1,20 @@
+void main(){
+//   Using where()
+  var evenNumbers = [1, -2, 3, 42].where((number) => number.isEven);
+
+  for (var number in evenNumbers) {
+    print('$number is even.');
+  }
+
+  if (evenNumbers.any((number) => number.isNegative)) {
+    print('evenNumbers contains negative numbers.');
+  }
+//  Using takeWhile
+  var numbers = [1, 3, -2, 0, 4, 5];
+
+  var numbersUntilZero = numbers.takeWhile((number) => number != 0);
+  print('Numbers until 0: $numbersUntilZero');
+
+  var numbersAfterZero = numbers.skipWhile((number) => number != 0);
+  print('Numbers after 0: $numbersAfterZero');
+}
